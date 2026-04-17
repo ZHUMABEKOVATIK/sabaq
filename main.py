071ab989c1949261ea25e840e1808dcf7ab2cc8b
Atabek <atabek.atik.808@gmail.com>
2026-04-17 16:25:47 +0500

    start

---
+++ main.py
@@ -0,0 +1,12 @@
+# Data types - Mag'liwmat tu'rleri
+
+# Integer - int - sanlarg'a - 5 9 7 6
+# String - str - tekstler - "Hello" 'Hello'
+# Float - float - bo'lshek sanlar - 5.6 8.9 1.2 1.5
+# Boolean - bool - logikaliq mag'liwmatlar - True False  - False, 0, 0.0, "", None
+# Complex - complex - matematik sanlar - 4j, 8j, 9j
+# List - list - [5, 8, 3]
+# Tuple - tuple - (3, 5)
+# Set - set - {5, 6, 7}
+# Dictionary - dict - {'key1': 'value1', 'key2': 'value2'}
+
